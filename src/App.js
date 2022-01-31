2a5f8261ae658e3f6aeec0d0848ecbe39024088f
Julia White <juliaspc@bobcat-124-64.bates.edu>
2022-01-30 19:02:10 -0500

    does this work?

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <head>
+      </head>
+      <body>
+        <h1>Hi! My name is Julia.</h1>
+        <h3>Some of my interests include:</h3>
+        <ul>
+            <li>running</li>
+            <li>biking</li>
+            <li>ice hockey</li>
+            <li>reading</li>
+            <li>physics</li>
+        </ul>
+        <p>I run on the Bates cross country and track teams!</p>
+        <h3>A little about me:</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            I am from Orono, ME.  It's a small town about 2 hours north of Lewiston and is the home of UMaine.
+            I am 18 and have a 16 year old younger sister.  We love to go on bike rides together!
+            My favorite season is winter because of the beautiful snow.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>I really do love the snow :)</p>
+        <h3>Future Plans?</h3>
+        <p>
+            I currently want to go into engineering but my main life dreams center around travel.
+            I hope to bike across the US and bikepack in Europe and the Middle East when I'm older.
+        </p>
+        <p>A few summers ago my sister and I travelled around PEI, exploring the islands's beautiful beaches.</p>
+      </body>
     </div>
   );
 }
