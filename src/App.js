7b008f2e086b214b449448270b7d1a494e3cf5b2
Julia White <juliaspc@bobcat-124-64.bates.edu>
2022-02-10 17:13:22 -0500

    submitting hw3

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { component } from 'react';
+import { MenuItems } from './components/Navbar/MenuItems';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Navbar />
       <head>
       </head>
       <body>
-        <h1>Hi! My name is Julia.</h1>
-        <h3>Some of my interests include:</h3>
-        <ul>
-            <li>running</li>
-            <li>biking</li>
-            <li>ice hockey</li>
-            <li>reading</li>
-            <li>physics</li>
+        <h1 className="header">Hi! My name is Julia.</h1>
+        <h3 className="header">Some of my interests include:</h3>
+        <ul className="about">
+            <li className="activity">running</li>
+            <li className="activity">biking</li>
+            <li className="activity">ice hockey</li>
+            <li className="activity">reading</li>
+            <li className="activity">physics</li>
         </ul>
-        <p>I run on the Bates cross country and track teams!</p>
-        <h3>A little about me:</h3>
-        <p>
+        <h3 className="header">A little bit of my background information:</h3>
+        <p className="about"> 
             I am from Orono, ME.  It's a small town about 2 hours north of Lewiston and is the home of UMaine.
             I am 18 and have a 16 year old younger sister.  We love to go on bike rides together!
-            My favorite season is winter because of the beautiful snow.
+            My favorite season is winter because I think the snow is beautiful.
         </p>
-        <p>I really do love the snow :)</p>
-        <h3>Future Plans?</h3>
-        <p>
+        <h3 className="header">Future Plans?</h3>
+        <p className="about">
             I currently want to go into engineering but my main life dreams center around travel.
             I hope to bike across the US and bikepack in Europe and the Middle East when I'm older.
         </p>
-        <p>A few summers ago my sister and I travelled around PEI, exploring the islands's beautiful beaches.</p>
+        <p className="hehe">
+          For a little more information about me, you can see that I put in the effort to make an entirely usless navbar.
+          I just felt like it was important stylistically but I don't have the content to include numerous different pages.
+          I hope that you enjoyed it too!
+        </p>
       </body>
     </div>
   );
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'This',
+        url: '#',
+        cName: 'nav-links',  
+    },
+    {
+        title: 'navbar',
+        url: '#',
+        cName: 'nav-links',  
+    },
+    {
+        title: 'is',
+        url: '#',
+        cName: 'nav-links',  
+    },
+    {
+        title: 'just',
+        url: '#',
+        cName: 'nav-links',  
+    },
+    {
+        title: 'for',
+        url: '#',
+        cName: 'nav-links-mobile',  
+    },
+]
